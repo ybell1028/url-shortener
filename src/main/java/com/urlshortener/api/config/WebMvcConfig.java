2a2a2d75e22082a3321ea847ac939c39193a229e
황종훈 <ybell1028@gmail.com>
2021-11-01 18:23:23 +0900

    Chores : Swagger 세팅 추가

---
+++ src/main/java/com/urlshortener/api/config/WebMvcConfig.java
@@ -0,0 +1,15 @@
+package com.urlshortener.api.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@RequiredArgsConstructor
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html");
+    }
+}

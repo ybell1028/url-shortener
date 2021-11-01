68e025c216d99c0c10f4a7569bb0afb82cff9465
황종훈 <ybell1028@gmail.com>
2021-11-01 15:25:40 +0900

    Chores : 기본 세팅 및 Health Check

--- src/main/java/com/urlshortener/api/UrlShortenerApplication.java
+++ src/main/java/com/urlshortener/api/UrlShortenerApplication.java
@@ -1,13 +1,11 @@
-package com.urlshortener.urlshortener;
+package com.urlshortener.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class UrlShortenerApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(UrlShortenerApplication.class, args);
     }
-
 }
---
+++ src/main/java/com/urlshortener/api/controller/HealthCheckController.java
@@ -0,0 +1,16 @@
+package com.urlshortener.api.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class HealthCheckController {
+    @GetMapping("")
+    public ResponseEntity<String> healthCheck() {
+        String message = "Welcome to url-shortener API! :)";
+        return ResponseEntity.status(200).body(message);
+    }
+}

febd90be4532ea1530e073847d6985e99e1fe02a
황종훈 <ybell1028@gmail.com>
2021-11-04 01:11:56 +0900

    Add : 프론트 페이지 추가

---
+++ src/main/resources/static/js/app/index.js
@@ -0,0 +1,28 @@
+const main = {
+    init: function () {
+        let _this = this;
+
+        $("#btn-convert").on("click", () => {
+            _this.save();
+        });
+    },
+    save: () => {
+        let data = {
+            originalUrl: $("#originalUrl").val(),
+        };
+
+        $.ajax({
+            type: "POST",
+            url: "/api/v1/urls",
+            dataType: "json",
+            contentType: "application/json; charset=utf-8",
+            data: JSON.stringify(data),
+        }).done((data) => {
+            $("#shortenedUrl").val(data.data.shortenedUrl);
+        }).fail((error) => {
+            alert(JSON.stringify(error));
+        });
+    }
+};
+
+main.init();

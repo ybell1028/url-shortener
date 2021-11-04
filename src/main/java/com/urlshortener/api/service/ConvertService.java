86107e8f779357c7a7a962eff88b52a0488a4ad3
황종훈 <ybell1028@gmail.com>
2021-11-04 20:27:06 +0900

    Refactor : UrlConvertService -> ConvertService로 이름 변경

--- src/main/java/com/urlshortener/api/service/ConvertService.java
+++ src/main/java/com/urlshortener/api/service/ConvertService.java
@@ -4,7 +4,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
-public class UrlConvertService {
+public class ConvertService {
     @Value("${values.dummy}")
     private int dummy;
     private final int BASE = 62;
--- src/main/java/com/urlshortener/api/service/UrlSerivce.java
+++ src/main/java/com/urlshortener/api/service/UrlSerivce.java
@@ -13,13 +13,14 @@ import java.util.Optional;
 @Service
 @RequiredArgsConstructor
 public class UrlSerivce {
-    private final UrlConvertService urlConvertService;
+    private final ConvertService convertService;
     private final UrlRepository urlRepository;
 
     @Transactional
     public Url saveAndUpdate(String originalUrl, String hostUrl) {
-        Url savedUrl = urlRepository.save(Url.builder().originalUrl(originalUrl).build());
-        String shortenUrl = urlConvertService.encode(savedUrl.getId());
+        Url newUrl = Url.builder().originalUrl(originalUrl).build();
+        Url savedUrl = urlRepository.save(newUrl);
+        String shortenUrl = convertService.encode(savedUrl.getId());
         savedUrl.update(hostUrl + shortenUrl);
         return urlRepository.save(savedUrl);
     }
@@ -31,7 +32,7 @@ public class UrlSerivce {
 
     @Transactional(readOnly = true)
     public String redirectShortenedUrl(String shortened) {
-        Url url = urlRepository.findById(urlConvertService.decode(shortened))
+        Url url = urlRepository.findById(convertService.decode(shortened))
                 .orElseThrow(() -> new ShortenerException(ShortenerError.BAD_REQUEST, "존재하지 않는 단축 URL 입니다."));
         return url.getOriginalUrl();
     }
